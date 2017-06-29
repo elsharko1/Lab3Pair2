7ab6aee1ba5834820dcd2eb3790abe0827148894
elsharko1 <elsharko1@gmail.com>
2017-06-28 23:50:39 -0400

    stumped with continue and garbage line crap

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        /*int userNum;
-
-        Scanner scan = new Scanner(System.in);
-
-        System.out.print("Enter an integer:");
-        userNum = scan.nextInt();
-
-
-        System.out.println("Number      Squared     Cubed");
-        System.out.println("======      =======     =====");
-
-        for (int i = 1; i <= userNum; i++) {
-            System.out.println(i + "            " + (i * i) +
-                    "          " + (i * i * i));
-        }
-        */
-
 
         int userNum;
         String choice = "y";
@@ -31,31 +14,34 @@ public class Main {
 
         System.out.println("Number" + "\t" + "Squared" + "\t" + "Cubed");
         System.out.println("======" + "\t" + "=======" + "\t" + "=====");
-        //System.out.println("hi");
 
-        //put this in a do while loop
+       /*do {
+        the square code stuff
 
-        /*do {
+        }
+        while (continue answer is yes);
+        or we
+        sout goodbye
 
-        }*/
+
+    */
+        scan.nextLine();
+
+        do {
         for (int i = 1; i <= userNum; i++) {
             System.out.println(i + "\t\t" + (i * i) +
                     "\t\t" + (i * i * i));
         }
-        scan.nextLine();
 
+        }
 
-        /*
-        while (choice.equalsIgnoreCase("y")){
+       while (choice.equalsIgnoreCase("y"));
 
             //get some input from the user and do stuff
             System.out.println("Continue? (y/n)");
             choice = scan.nextLine();
-        }
-        System.out.println("Goodbye");
-        */
 
-        // write your code here
+        System.out.println("Goodbye");
 
     }
 }
